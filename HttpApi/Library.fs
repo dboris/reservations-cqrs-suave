d134f50cb3e8d20264efd682dc5d788b352c01c5
Boris D. <boris.dob@gmail.com>
2017-09-09 09:26:05 -0700

    Init project.

---
+++ HttpApi/Library.fs
@@ -0,0 +1,4 @@
+namespace HttpApi
+
+module Say = 
+    let hello name = printfn "Hello %s" name
---
+++ WebHost/Program.fs
@@ -0,0 +1,7 @@
+﻿// Learn more about F# at http://fsharp.org
+open System
+
+[<EntryPoint>]
+let main argv = 
+    HttpApi.Say.hello "bob"
+    0
