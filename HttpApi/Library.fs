20dac4fe7eec34eb5bc6582ff19ec1dfe7a9f9ec
Boris D. <boris.dob@gmail.com>
2017-09-09 12:25:28 -0700

    Publish reservation command messages.

--- HttpApi/Library.fs
+++ HttpApi/Library.fs
@@ -1,8 +1,43 @@
 namespace HttpApi
 
+open System
+open System.Reactive.Subjects
+
 open Suave
 open Suave.Filters
 open Suave.Operators
 
+// JsonFormatter.SerializerSettings.ContractResolver <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver
+
+// The controller for reservations can publish MakeReservation commands
+module Reservations =
+    let subject = new Subject<Envelope<MakeReservation>> ()  // Dispose?
+    let reservationsObservable =
+        { new IObservable<Envelope<MakeReservation>> with
+            member __.Subscribe observer = subject.Subscribe observer }
+
+    [<CLIMutable>]
+    type MakeReservationRendition =
+      { Date : string
+        Name : string
+        Email : string
+        Quantity : int }
+
+    let postReservation (rendition : MakeReservationRendition) =
+        let cmd : Envelope<MakeReservation> =
+            { MakeReservation.Date = DateTime.Parse rendition.Date
+              Name = rendition.Name
+              Email = rendition.Email
+              Quantity = rendition.Quantity 
+            } 
+            |> envelopWithDefaults
+        subject.OnNext cmd
+
+    let routes = 
+        path "/res" >=> POST >=> Successful.ACCEPTED "Accepted"
+
 module Api = 
-    let main = choose [ GET >=> Successful.OK "Hello" ]
+    let main = 
+        choose 
+          [ GET >=> Successful.OK "Hello" 
+            Reservations.routes ]
---
+++ HttpApi/Messages.fs
@@ -0,0 +1,26 @@
+﻿namespace HttpApi
+
+open System
+
+// The HttpApi is a Message Endpoint. Need to convert Http traffic to messages. Put each message in an envelope.
+[<CLIMutable>]
+type MakeReservation =
+  { Date : DateTime
+    Name : string
+    Email : string
+    Quantity : int }
+
+[<AutoOpen>]
+module Envelope =
+    [<CLIMutable>]
+    type Envelope<'T> =
+      { Id : Guid
+        Created : DateTimeOffset
+        Item : 'T }
+
+    let envelop id created item =
+      { Id = id; Created = created; Item = item }
+
+    let envelopWithDefaults item =
+        envelop (Guid.NewGuid ()) DateTimeOffset.Now item
+
