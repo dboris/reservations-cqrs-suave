485fde8ab153efea8ecfacad1abe1c928b3e5211
Boris D. <boris.dob@gmail.com>
2017-09-10 12:58:22 -0700

    Add reservations agent.

--- HttpApi/Library.fs
+++ HttpApi/Library.fs
@@ -8,12 +8,18 @@ open Suave.Filters
 open Suave.Operators
 
 
+type Agent<'T> = FSharp.Control.MailboxProcessor<'T>
+
 // The controller for reservations can publish MakeReservation commands
 module ReservationsController =
+    open Reservations
+
     let subject = new Subject<Envelope<MakeReservation>> ()  // Dispose?
     let reservationsObservable =
         { new IObservable<Envelope<MakeReservation>> with
             member __.Subscribe observer = subject.Subscribe observer }
+    let seatingCapacity = 10
+    let reservations = Collections.Concurrent.ConcurrentBag<Envelope<Reservation>> ()
 
     [<CLIMutable>]
     type MakeReservationRendition =
@@ -31,9 +37,27 @@ module ReservationsController =
             } 
             |> envelopWithDefaults
         subject.OnNext cmd
+        Successful.ACCEPTED "Accepted"
 
+    let agent = new Agent<Envelope<MakeReservation>> (fun inbox ->
+        let rec loop () =
+            async {
+                let! cmd = inbox.Receive ()
+                let rs = reservations |> toReservations
+                match handle seatingCapacity rs cmd with
+                | Some r -> 
+                    reservations.Add r
+                    printfn "Added reservation: %A" r
+                    printfn "All: %A" reservations
+                | None -> ()
+                return! loop () }
+        loop ())
+    agent.Start ()
+    let sub = reservationsObservable.Subscribe agent.Post  // Dispose?
     let routes = 
-        path "/res" >=> POST >=> Successful.ACCEPTED "Accepted"
+        path "/res" 
+        >=> POST 
+        >=> request (getResourceFromReq >> postReservation)
 
 module Api = 
     let main = 
