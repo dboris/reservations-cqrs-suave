69339f6bf0d9d7cfce078372cc4593ed699e97ed
Boris D. <boris.dob@gmail.com>
2017-09-17 12:55:11 -0700

    Decouple agent from adding reservation to store.

--- WebHost/Program.fs
+++ WebHost/Program.fs
@@ -10,15 +10,16 @@ let seatingCapacity = 10
 
 type Agent<'T> = FSharp.Control.MailboxProcessor<'T>
 
+let reservationSubject = new Subjects.Subject<Envelope<Reservation>> ()
+
 // This agent serializes the processing of reservation requests, so they happen sequentially
 let agent = new Agent<Envelope<MakeReservation>> (fun inbox ->
-    let repo = reservationsStore :> Db.IReservationsRepo
     let rec loop () =
         async {
             let! cmd = inbox.Receive ()
-            let rs = repo.ToReservations
+            let rs = (reservationsStore :> Db.IReservationsRepo).ToReservations
             match Reservations.handle seatingCapacity rs cmd with
-            | Some r -> repo.Add r
+            | Some r -> reservationSubject.OnNext r
             | None -> ()
             return! loop () }
     loop ())
@@ -27,6 +28,7 @@ let reservationRequestObserver = Observer.Create (fun m -> agent.Post m)
 
 [<EntryPoint>]
 let main argv =
+    reservationSubject.Subscribe (reservationsStore :> Db.IReservationsRepo).Add |> ignore
     agent.Start ()
     startWebServer serverConfig (Api.main reservationsStore reservationRequestObserver)
     0
